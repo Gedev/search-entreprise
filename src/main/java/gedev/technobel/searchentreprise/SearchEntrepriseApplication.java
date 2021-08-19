4be45be6059ef590a18983a82c2a0e5d42baeb9a
Gérald Devreux <gerald.devreux.pro@gmail.com>
2021-08-19 15:38:45 +0200

    First commit

---
+++ src/main/java/gedev/technobel/searchentreprise/SearchEntrepriseApplication.java
@@ -0,0 +1,13 @@
+package gedev.technobel.searchentreprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchEntrepriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchEntrepriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gedev/technobel/searchentreprise/SearchEntrepriseApplicationTests.java
@@ -0,0 +1,13 @@
+package gedev.technobel.searchentreprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchEntrepriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
