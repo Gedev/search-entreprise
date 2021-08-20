7a4259eb99a7e53d2f67486505f463cdfc3fbe7e
Gérald Devreux <gerald.devreux.pro@gmail.com>
2021-08-20 11:03:28 +0200

    SQL connection (dependency + property added)

--- src/main/java/gedev/technobel/searchentreprise/SearchEntrepriseApplication.java
+++ src/main/java/gedev/technobel/searchentreprise/SearchEntrepriseApplication.java
@@ -3,11 +3,25 @@ package gedev.technobel.searchentreprise;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class SearchEntrepriseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SearchEntrepriseApplication.class, args);
-	}
 
+		try {
+			String url = "jdbc:mariadb://localhost:3306/search-entreprise";
+			String user = "root";
+			String password = "";
+
+			DriverManager.getConnection(url, user, password);
+			System.out.println("Connection has been successfully established with the database !");
+
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		};
+	}
 }
